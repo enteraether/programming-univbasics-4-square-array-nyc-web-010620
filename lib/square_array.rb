c84ec55babf6e2447e609dbc69e1b3f45a1a2c6d
Theresa <thetheresagarcia@gmail.com>
2019-12-12 16:38:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,20 @@
+# def square_array(array)
+#   squared[]
+#   count = 0
+#  while array.length > count do
+#   squared << (array[count] ** 2)
+#   count += 1
+# end
+#  squared
+# end
+
+
 def square_array(array)
-  # your code here
+  squared[]
+  count = 1
+ while array.length >= count do
+  squared << (array[count] ** 2)
+  count += 1
+end
+ squared
 end
